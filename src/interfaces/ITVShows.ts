c10c8fab2c333fac2af661b7c63462550c1b394f
Raymond Lam <raymond.m.lam@gmail.com>
2022-07-29 11:05:19 +0200

    interfaces for resources added

---
+++ src/interfaces/ITVShows.ts
@@ -0,0 +1,15 @@
+export interface ITVShows {
+	cover_url: string;
+	directed_by: string;
+	id: number;
+	imdb_id: string;
+	last_aired_date: string;
+	number_episodes: number;
+	number_seasons: number;
+	overview: string;
+	phase: number;
+	release_date: string;
+	saga: string;
+	title: string;
+	trailer_url: string;
+}
