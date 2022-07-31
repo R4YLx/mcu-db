71f52c603a20bb0a3a3dcdf00977ab8b2ececbf2
Raymond Lam <raymondlam@Raymonds-MacBook-Air.local>
2022-07-31 13:18:34 +0200

    test

--- src/pages/Movies.tsx
+++ src/pages/Movies.tsx
@@ -2,10 +2,27 @@ import { useQuery } from "@tanstack/react-query";
 import { getMovies } from "../services/MCUAPI";
 
 const Movies = () => {
-	const { data } = useQuery(["movies"], getMovies);
+	const { data, isSuccess } = useQuery(["movies"], getMovies);
 
 	console.log("Movies:", data);
-	return <div>Movies</div>;
+
+	console.log(typeof data);
+
+	// let allMyMovies = data[0].map((movie) => {
+	// 	return (
+	// 		<div>
+	// 			<p>{movie.title}</p>
+	// 		</div>
+	// 	);
+	// });
+
+	return (
+		// <div>{isSuccess && data.map((movie) => <div>{movie.title}</div>)}</div>
+		<div>
+			<p>Tjena</p>
+			{/* {allMyMovies} */}
+		</div>
+	);
 };
 
 export default Movies;
