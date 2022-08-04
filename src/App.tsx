647ca2616bba8cbb759420e03122a881a706a051
Raymond Lam <raymond.m.lam@gmail.com>
2022-08-05 00:35:24 +0200

    added navbar

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,12 @@ import { Routes, Route } from "react-router-dom";
 import "./scss/App.scss";
 import Movies from "./pages/Movies";
 import TVShows from "./pages/TVShows";
+import Navbar from "./components/Navbar";
 
 function App() {
 	return (
 		<div className="App bg-gradient-to-t from-gray-900 to-zinc-900">
-			<h1 className="text-3xl font-bold text-center">MCU-DB</h1>
+			<Navbar />
 
 			<Routes>
 				<Route path="/movies" element={<Movies />} />
