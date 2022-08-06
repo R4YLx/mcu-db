b4d78450b5ba264718c7c6eea17daa9fdbda8842
Raymond Lam <raymond.m.lam@gmail.com>
2022-08-07 01:47:51 +0200

    style pages

--- src/pages/MovieDetails.tsx
+++ src/pages/MovieDetails.tsx
@@ -1,14 +1,56 @@
 import { useQuery } from "@tanstack/react-query";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { getSingleMovie } from "../services/MCUAPI";
 
 const MovieDetails = () => {
 	const { id } = useParams();
 	const { data } = useQuery(["movie", { id }], getSingleMovie);
+	const navigate = useNavigate();
 
 	console.log("id", id);
 	console.log("Clicked movie", data);
-	return <div>MovieDetails</div>;
+	return (
+		<div>
+			<div className="card w-96 bg-base-100 shadow-xl image-full">
+				<figure>
+					<img src={data?.cover_url} alt="cover" />
+				</figure>
+				<div className="card-body">
+					<h2 className="card-title">{data?.title}</h2>
+					<p>
+						<b> Synopsis: </b>
+						<br /> {data?.overview}
+					</p>
+					<p>
+						<b> Release date:</b> {data?.release_date}
+					</p>
+					<p>
+						<b>Directed by: </b>
+						{data?.directed_by}{" "}
+					</p>
+					<p>
+						<b>Duration: </b>
+						{data?.duration}{" "}
+					</p>
+
+					<div>
+						<p>
+							<b>Related movies:</b>
+						</p>
+						<ul></ul>
+					</div>
+					<div className="card-actions justify-end">
+						<button
+							className="btn bg-[#ED1D24] btn-outline text-white"
+							onClick={() => navigate(-1)}
+						>
+							Back
+						</button>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default MovieDetails;
--- src/pages/TVShowsPage.tsx
+++ src/pages/TVShowsPage.tsx
@@ -1,4 +1,5 @@
 import { useQuery } from "@tanstack/react-query";
+import { Link } from "react-router-dom";
 import { IDataTvShows } from "../interfaces/IDataTvShows";
 import { getTvShows } from "../services/MCUAPI";
 
@@ -12,7 +13,22 @@ const TVShowsPage = () => {
 			{isSuccess &&
 				data?.data.map((tvshow: any) => (
 					<div key={tvshow.id}>
-						<h3>{tvshow.title}</h3>
+						<div className="card m-8 min-h-max">
+							<figure>
+								<img src={tvshow.cover_url} alt="poster" className="w-full" />
+							</figure>
+							<div className="card-body bg-base-100 shadow-xl">
+								<h2 className="card-title">{tvshow.title}</h2>
+								<p className="truncate italic">Synopsis: {tvshow.overview}</p>
+								<Link to={`/tvshows/${tvshow.id}`}>
+									<div className="card-actions justify-end">
+										<button className="btn bg-[#ED1D24] btn-outline text-white mt-4">
+											Read more
+										</button>
+									</div>
+								</Link>
+							</div>
+						</div>
 					</div>
 				))}
 		</main>
