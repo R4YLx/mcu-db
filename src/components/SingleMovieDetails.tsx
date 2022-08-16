36431661d1a7a14aabcfc1cd4c24e0da1b562495
Raymond Lam <raymond.m.lam@gmail.com>
2022-08-16 23:28:49 +0200

    created movie details component

---
+++ src/components/SingleMovieDetails.tsx
@@ -0,0 +1,75 @@
+import { useNavigate } from "react-router-dom";
+import { IMovie } from "../interfaces/IDataMovies";
+
+type Movie = {
+	data: IMovie;
+};
+
+const SingleMovieDetails = ({ data }: Movie) => {
+	const navigate = useNavigate();
+
+	return (
+		<div className="card w-96 bg-base-100 shadow-xl image-full">
+			<figure>
+				<img src={data?.cover_url} alt="cover" />
+			</figure>
+
+			<div className="card-body">
+				<h2 className="card-title">{data?.title}</h2>
+
+				<div className="divider">Synopsis</div>
+
+				<p>{data?.overview}</p>
+
+				<div className="divider"></div>
+
+				<p>
+					<b> Release date:</b> {data?.release_date}
+				</p>
+				<p>
+					<b>Directed by: </b>
+					{data?.directed_by}
+				</p>
+				<p>
+					<b>Duration: </b>
+					{data?.duration} minutes
+				</p>
+				<p>
+					<b>Box office: </b>${data?.box_office}
+				</p>
+				<p>
+					<b>Post credit scenes: </b>
+					{data?.post_credit_scenes}
+				</p>
+				<p>
+					<b>Saga: </b>
+					{data?.saga}
+				</p>
+
+				<div className="divider">Trailer</div>
+				<iframe src={data?.trailer_url}></iframe>
+
+				<div className="divider">Related movies:</div>
+				<div>
+					<ul>
+						{data?.related_movies?.map((rel_movie) => (
+							<li key={rel_movie.id} className="list-disc list-inside">
+								{rel_movie.title}
+							</li>
+						))}
+					</ul>
+				</div>
+				<div className="card-actions justify-end">
+					<button
+						className="btn bg-[#ED1D24] btn-outline text-white"
+						onClick={() => navigate(-1)}
+					>
+						Back
+					</button>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default SingleMovieDetails;
--- src/pages/MovieDetails.tsx
+++ src/pages/MovieDetails.tsx
@@ -1,17 +1,13 @@
 import { useEffect, useState } from "react";
-import { useNavigate, useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
+import SingleMovieDetails from "../components/SingleMovieDetails";
 import { IMovie } from "../interfaces/IDataMovies";
 import MCUAPI from "../services/MCUAPI";
-
-type DataDetails = {
-	id: number;
-};
 
 const MovieDetails = () => {
 	const [data, setData] = useState<IMovie>();
 	const [isLoading, setIsLoading] = useState<boolean>(false);
 	const params = useParams();
-	const navigate = useNavigate();
 
 	const fetchSingleMovie = async () => {
 		setIsLoading(true);
@@ -26,44 +22,9 @@ const MovieDetails = () => {
 
 	return (
 		<div>
-			<div className="card w-96 bg-base-100 shadow-xl image-full">
-				<figure>
-					<img src={data?.cover_url} alt="cover" />
-				</figure>
-				<div className="card-body">
-					<h2 className="card-title">{data?.title}</h2>
-					<p>
-						<b> Synopsis: </b>
-						<br /> {data?.overview}
-					</p>
-					<p>
-						<b> Release date:</b> {data?.release_date}
-					</p>
-					<p>
-						<b>Directed by: </b>
-						{data?.directed_by}{" "}
-					</p>
-					<p>
-						<b>Duration: </b>
-						{data?.duration}{" "}
-					</p>
+			{isLoading && <h1>LOADING...</h1>}
 
-					<div>
-						<p>
-							<b>Related movies:</b>
-						</p>
-						<ul></ul>
-					</div>
-					<div className="card-actions justify-end">
-						<button
-							className="btn bg-[#ED1D24] btn-outline text-white"
-							onClick={() => navigate(-1)}
-						>
-							Back
-						</button>
-					</div>
-				</div>
-			</div>
+			{data && <SingleMovieDetails data={data} />}
 		</div>
 	);
 };
