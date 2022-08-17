4eeabc1ad5ddfeb5c0ba6798db250039552fc146
Raymond Lam <raymond.m.lam@gmail.com>
2022-08-17 21:31:51 +0200

    centering loading spinner

--- src/pages/TVShowsPage.tsx
+++ src/pages/TVShowsPage.tsx
@@ -20,11 +20,15 @@ const TVShowsPage = (): JSX.Element => {
 	}, []);
 
 	return (
-		<main className="grid grid-cols-1 gap-8 py-8 px-8 justify-items-center md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-6">
-			{isLoading && <LoadingSpinner />}
+		<>
+			<div className="flex justify-center items-center">
+				{isLoading && <LoadingSpinner />}
+			</div>
 
-			{data && <TvShowCard data={data.data} />}
-		</main>
+			<main className="grid grid-cols-1 gap-8 py-8 px-8 justify-items-center md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-6">
+				{data && <TvShowCard data={data.data} />}
+			</main>
+		</>
 	);
 };
 
