8152c78bc7f583ed725e48d3b7ec002be7cd0f4e
Raymond Lam <raymond.m.lam@gmail.com>
2022-08-17 14:28:52 +0200

    Welcome page content

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,5 +1,17 @@
 const HomePage = () => {
-	return <div></div>;
+	return (
+		<div className="hero min-h-screen">
+			<div className="hero-overlay bg-opacity-60"></div>
+			<div className="hero-content text-center text-neutral-content">
+				<div className="max-w-md">
+					<h1 className="mb-5 text-5xl font-bold">Welcome</h1>
+					<p className="mb-5">
+						This is a database with MCU related movies and TV shows.
+					</p>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default HomePage;
